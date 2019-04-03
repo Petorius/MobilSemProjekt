ce1594308f27e3a66092f0be72a08be72875fe71
Peter von Hohenzollern <petorius@hotmail.com>
2019-04-03 11:29:08 +0200

    Tilføjede Google Maps kort.

--- MobilSemProjekt/MobilSemProjekt.Android/MainActivity.cs
+++ MobilSemProjekt/MobilSemProjekt.Android/MainActivity.cs
@@ -1,25 +1,52 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
+using Android;
+using Android.Util;
+using Permission = Android.Content.PM.Permission;
+using System.IO;
 
-namespace MobilSemProjekt.Droid
-{
-    [Activity(Label = "MobilSemProjekt", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
-    {
-        protected override void OnCreate(Bundle savedInstanceState)
+namespace MobilSemProjekt.Droid {
+    [Activity(Label = "XamarinMapsGarbage", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity {
+
+        readonly string[] PermissionsLocation =
         {
+    Manifest.Permission.AccessCoarseLocation,
+    Manifest.Permission.AccessFineLocation
+    };
+
+        const int RequestLocationId = 0;
+        protected override void OnCreate(Bundle savedInstanceState) {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
-
             base.OnCreate(savedInstanceState);
+
+
+            const string permission = Manifest.Permission.AccessFineLocation;
+            if (CheckSelfPermission(permission) == (int)Permission.Granted) {
+                Log.Error("ERROR", "GRANTED");
+            }
+            else {
+                Log.Error("ERROR", "DENIED");
+                RequestPermissions(PermissionsLocation, RequestLocationId);
+                if (CheckSelfPermission(permission) == (int)Permission.Granted) {
+                    Log.Error("ERROR", "NOW ITS GRANTED");
+                }
+                else {
+                    Log.Error("ERROR", "STILL DENIED");
+                }
+            }
+            string dbName = "Marker.db";
+            string folderPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            string dbPath = Path.Combine(folderPath, dbName);
+
+            Xamarin.FormsGoogleMaps.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+
+
         }
     }
 }
--- MobilSemProjekt/MobilSemProjekt/MainPage.xaml.cs
+++ MobilSemProjekt/MobilSemProjekt/MainPage.xaml.cs
@@ -1,14 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.GoogleMaps;
+using Plugin.Geolocator;
+using Xamarin.Forms.Internals;
+using SQLite;
+using Xamarin.Essentials;
+using System.Linq;
+
 
 namespace MobilSemProjekt {
     public partial class MainPage : ContentPage {
         public MainPage() {
             InitializeComponent();
+
+
+
+            Content = new StackLayout() {
+                Spacing = 5,
+                Children =
+                {
+                    GGMAP,
+                    OurEntry
+                }
+
+            };
         }
     }
 }
