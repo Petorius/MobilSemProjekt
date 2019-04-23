1b1a7f0eb660e961f9a38ed33046e55cb7efd932
Peter von Hohenzollern <petorius@hotmail.com>
2019-04-23 11:04:55 +0200

    Tilføjede nyt feature; Der kan nu oprettes lokationer ud fra søgninger.

--- MobilSemProjekt/MobilSemProjekt/View/SearchListView.xaml.cs
+++ MobilSemProjekt/MobilSemProjekt/View/SearchListView.xaml.cs
@@ -9,6 +9,9 @@ using MobilSemProjekt.MVVM.Model;
 using MobilSemProjekt.MVVM.ViewModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Acr.UserDialogs;
+using Xamarin.Essentials;
+using Location = MobilSemProjekt.MVVM.Model.Location;
 
 namespace MobilSemProjekt.View
 {
@@ -42,14 +45,70 @@ namespace MobilSemProjekt.View
 
         async void Handle_ItemTapped(object sender, ItemTappedEventArgs e)
         {
-            Location location = (Location) e.Item;
-            MainPage mainPage = new MainPage();
-            await Navigation.PushAsync(mainPage);
-            mainPage.GoToLocation(location.Latitude, location.Longitude);
-            Debug.WriteLine("Gå til " + location.Latitude + " " + location.Longitude);
+            var itemTappedAnswer = await DisplayAlert("Lokation", "What do you want to do?", "Go to location", "Add location");
+            Location location = (Location)e.Item;
+
+
+            if (itemTappedAnswer)
+            {
+                MainPage mainPage = new MainPage();
+                await Navigation.PushAsync(mainPage);
+                mainPage.GoToLocation(location.Latitude, location.Longitude);
+                Debug.WriteLine("Gå til " + location.Latitude + " " + location.Longitude);
+            }
+
+            if (!itemTappedAnswer)
+            {
+                try
+                {
+                    string geocodeAddress;
+
+                    var placemarks = await Geocoding.GetPlacemarksAsync(location.Latitude, location.Longitude);
+
+                    var placemark = placemarks?.FirstOrDefault();
+                    if (placemark != null)
+                    {
+                        geocodeAddress =
+                            $"{placemark.CountryName}, " +
+                            $"{placemark.Locality}, " +
+                            $"{placemark.PostalCode}, " +
+                            $"{placemark.Thoroughfare} ";
+
+
+                        try
+                        {
+                            PromptResult pResult = await UserDialogs.Instance.PromptAsync(new PromptConfig
+                            {
+                                InputType = InputType.Name,
+                                OkText = "Add",
+                                Title = "Enter name for the location",
+                            });
+                            if (pResult.Ok && !string.IsNullOrWhiteSpace(pResult.Text))
+                            {
+                                location.LocationName = pResult.Text;
+                            }
+                        }
+                        catch (Exception exception)
+                        {
+                            Console.WriteLine(exception.StackTrace);
+                        }
+                    }
+                }
+
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.StackTrace);
+                    // Handle exception that may have occurred in geocoding
+                }
+
+                IRestService restService = new RestService();
+                await restService.Create(location);
+            }
 
             //Deselect Item
             ((ListView)sender).SelectedItem = null;
         }
+
     }
 }
